b533302d8efe3dba10ca13a85beb0a2b1150f94b
IgorKolpakov <yandex.ru>
2023-02-14 19:11:39 +0200

    work9.2

---
+++ work9.2.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+
+int main()
+{
+	bool flag = true;
+	std::cout << "Before:\n";
+	char surname[5][20] = {"Pavlov", "Starkov", "Begunov",  "Semenov", "Anisimov"};
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << surname[i] << "\n";
+	}
+
+	while (flag)
+	{
+		flag = false;
+		for (int i = 0; i < 5 - 1; i++)
+		{
+			if ((int) surname[i][0] > (int) surname[i + 1][0])
+			{
+				flag = true;
+				char temp[20]; 
+				strcpy(temp, surname[i]);
+				strcpy(surname[i],surname[i + 1]);
+				strcpy(surname[i + 1], temp);
+			}
+		}
+	}
+
+	std::cout << "\n\nAfter:\n";
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << surname[i] << "\n";
+	}
+}
